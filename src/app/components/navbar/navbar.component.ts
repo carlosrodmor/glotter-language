17a3fae5a7f35d8e7b5352f938981a23311d2544
Carlos Rodríguez <carlos.rodriguez@accuro.es>
2024-11-04 12:39:44 +0100

    contact componente created, implemented and modified navbar with logo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  isMenuOpen = false;
+  logoPath = 'assets/images/logo.png';
 
+  constructor(private router: Router) {}
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  redirectTo(route: string) {
+    this.router.navigate([`/${route}`]);
+  }
 }
