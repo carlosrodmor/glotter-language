ff54ac7adad02dc115188d944d5b01e25c061ce0
lardevpro <lardevpro@gmail.com>
2024-11-06 10:25:17 +0100

    component-login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
 ];
