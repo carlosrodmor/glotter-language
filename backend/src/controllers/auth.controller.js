ee5f81ed3ddf7f04dcc44a049ad2e3e473f19919
Carlos Rodríguez <carlos.rodriguez@accuro.es>
2024-11-06 13:41:25 +0100

    developing all backend

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,54 @@
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "El usuario ya existe" });
+    }
+
+    const user = new User({
+      name,
+      email,
+      password,
+    });
+
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Error en el servidor" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Credenciales inválidas" });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(401).json({ message: "Credenciales inválidas" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Error en el servidor" });
+  }
+};
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    languages: [
+      {
+        language: {
+          type: String,
+          required: true,
+        },
+        level: {
+          type: String,
+          enum: ["A1", "A2", "B1", "B2", "C1", "C2"],
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
